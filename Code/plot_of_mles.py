45fc50be61862f1e5608f6d68cebec43dd29b2a2
Johanne Skogvang <johanne_skogvang@hotmail.com>
2021-05-28 10:19:44 +0200

    div filer med plotting

--- Code/plot_of_mles.py
+++ Code/plot_of_mles.py
@@ -30,7 +30,7 @@ print(alpha_vec)
 #Not zoomed
 plt.scatter(alpha_vec,eta_vec,s=10,color='mediumseagreen')
 #plt.yscale('log') #for log scale of eta.
-plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
 plt.xlabel(r'$\alpha$')
 plt.ylabel(r'$\eta$')
 plt.show()
@@ -39,9 +39,9 @@ plt.show()
 #zoomed
 plt.scatter(alpha_vec,eta_vec,s=10,color='mediumseagreen')
 #plt.yscale('log') #for log scale of eta.
-plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
-plt.xlabel(r'$\alpha$')
-plt.ylabel(r'$\eta$')
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\alpha}$')
+plt.ylabel(r'$\hat{\eta}$')
 plt.xlim([-0.01,0.2])
 plt.ylim([-10,300])
 plt.show()
@@ -49,12 +49,96 @@ plt.show()
 #zoomed even more:
 plt.scatter(alpha_vec,eta_vec,s=10,color='mediumseagreen')
 #plt.yscale('log') #for log scale of eta.
-plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
-plt.xlabel(r'$\alpha$')
-plt.ylabel(r'$\eta$')
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\alpha}$')
+plt.ylabel(r'$\hat{\eta}$')
 plt.xlim([-0.01,0.075])
 plt.ylim([-2,75])
 plt.show()
+
+
+
+
+'''
+%%%%%%%%%%%%%%%%%%%%%%%%% LIMITED %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+'''
+
+path = r'C:\Users\Johan\OneDrive\Documents\Masteroppgave\Data\Gamma=kappa=1\bootstrap_limited.csv'
+df_unlim = pd.read_csv(path,sep=',')
+df_unlim=df_unlim.drop('Unnamed: 0',axis=1)
+
+
+eta_vec = list(df_unlim['eta'])
+print(eta_vec)
+
+alpha_vec = list(df_unlim['alpha'])
+print(alpha_vec)
+
+beta_vec = list(df_unlim['beta'])
+print(beta_vec)
+
+''' alpha and eta '''
+#Plotting eta and alpha values:
+#Not zoomed
+plt.scatter(alpha_vec,eta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\alpha}$')
+plt.ylabel(r'$\hat{\eta}$')
+plt.show()
+
+'''
+#zoomed
+plt.scatter(alpha_vec,eta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$\eta$')
+plt.xlim([-0.01,0.2])
+plt.ylim([0,20])
+plt.show()
+'''
+
+''' alpha and beta'''
+plt.scatter(alpha_vec,beta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$\beta$')
+plt.show()
+
+#zoomed in on beta axis. 
+plt.scatter(alpha_vec,beta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\alpha}$')
+plt.ylabel(r'$\hat{\beta}$')
+#plt.xlim([-0.01,0.2])
+plt.ylim([-0.1,1.1])
+plt.show()
+
+
+
+'''beta and eta '''
+plt.scatter(beta_vec,eta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\beta}$')
+plt.ylabel(r'$\hat{\eta}$')
+plt.show()
+
+
+plt.scatter(beta_vec,eta_vec,s=10,color='mediumseagreen')
+#plt.yscale('log') #for log scale of eta.
+#plt.title(r"MLE of $\alpha$ and $\eta$ for the 76 participants, unlimited. $\gamma=\kappa=1$.")
+plt.xlabel(r'$\hat{\beta}$')
+plt.ylabel(r'$\hat{\eta}$')
+plt.xlim([-0.1,1.3])
+plt.ylim([-5,20])
+plt.show()
+
+
+
 
 
 
--- Code/plots_for_thesis.py
+++ Code/plots_for_thesis.py
@@ -34,11 +34,26 @@ b=0.1
 prob01 = ss.beta.pdf(theta,a,b)
 print(prob01)
 
+a=2
+b=2
+prob2 = ss.beta.pdf(theta,a,b)
+
+a=5
+b=5
+prob5 = ss.beta.pdf(theta,a,b)
 
 
-plt.plot(theta,prob01)
-plt.plot(theta,prob05)
-plt.plot(theta,prob1)
+plt.plot(theta,prob01,color='darkorange')
+plt.plot(theta,prob05,color='darkorchid')
+plt.plot(theta,prob1,color='deeppink')
+plt.plot(theta,prob2,color='black')
+plt.plot(theta,prob5,color='grey')
+#plt.title('The beta distribution')
+plt.xlabel(r'$\theta$')
+plt.ylabel('Probability density')
+plt.legend([r'$\gamma=\kappa=0.1$', r'$\gamma=\kappa=0.5$',r'$\gamma=\kappa=1$',r'$\gamma=\kappa=2$',r'$\gamma=\kappa=5$'],bbox_to_anchor=(1.05, 1))
+#plt.legend([r'$\gamma=\kappa=0.1$', r'$\gamma=\kappa=0.5$',r'$\gamma=\kappa=1$',r'$\gamma=\kappa=2$',r'$\gamma=\kappa=5$'],
+#           bbox_to_anchor=(0., 1.02, 1., .102),ncol=5,loc='center')
 plt.show()
 
 
@@ -51,40 +66,83 @@ data = pd.read_csv(r'C:\Users\Johan\OneDrive\Documents\Masteroppgave\Data\data1.
 data = data.rename(columns={'Unnamed: 0':'ID'}) #putting ID as the name of the first column. 
 data.head()
 
-print(data['BoxNormExtDtd2'])
+print(data['BoxNormExtDtD2'])
 
 
 
+trial2 = data['BoxNormExtDtD2']
+trial2 = trial2.tolist()
+plt.hist(trial2,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5],color='mediumseagreen')
+plt.title('Trial 2')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
 
+trial3 = data['BoxNormExtDtD3']
+trial3 = trial3.tolist()
+plt.hist(trial3,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5],color='mediumseagreen')
+plt.title('Trial 3')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
+
+trial4 = data['BoxNormExtDtD4']
+trial4 = trial4.tolist()
+plt.hist(trial4,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5],color='mediumseagreen')
+plt.title('Trial 4')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
 
+trial5 = data['BoxNormExtDtD5']
+trial5 = trial5.tolist()
+plt.hist(trial5,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5],color='mediumseagreen')
+plt.title('Trial 5')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
+trial6 = data['BoxNormExtDtD6']
+trial6 = trial6.tolist()
+plt.hist(trial6,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5],color='mediumseagreen')
+plt.title('Trial 6')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
+trial7 = data['BoxNormExtDtD7']
+trial7 = trial7.tolist()
+plt.hist(trial7,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5],color='mediumseagreen')
+plt.title('Trial 7')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
+trial8 = data['BoxNormExtDtD8']
+trial8 = trial8.tolist()
+plt.hist(trial8,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5],color='mediumseagreen')
+plt.title('Trial 8')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
+trial9 = data['BoxNormExtDtD9']
+trial9 = trial9.tolist()
+plt.hist(trial9,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5],color='mediumseagreen')
+plt.title('Trial 9')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+trial10 = data['BoxNormExtDtD10']
+trial10 = trial10.tolist()
+plt.hist(trial10,bins=[-0.5,0.5,1.5,2.5,3.5,4.5,5.5,6.5],color='mediumseagreen')
+plt.title('Trial 10')
+plt.xlabel('Draws to decision')
+plt.ylabel('Number of participants')
+plt.show
 
 
 
